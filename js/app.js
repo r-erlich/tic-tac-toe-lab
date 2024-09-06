3d7753b46146c67f28221956179fa4691a24554d
Reuben <grerlich@gmail.com>
2024-09-06 09:46:18 -0400

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,9 +1,15 @@
 /*-------------------------------- Constants --------------------------------*/
-
+const squareEls = document.querySelectorAll("#sqr")
+const messageEl = document.querySelector("#message")
 /*---------------------------- Variables (state) ----------------------------*/
-
+let board;
+let turn;
+let winner;
+let tie;
 /*------------------------ Cached Element References ------------------------*/
 
 /*-------------------------------- Functions --------------------------------*/
-
+const init() => {
+    console.log()
+}
 /*----------------------------- Event Listeners -----------------------------*/
